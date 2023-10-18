1561a70287394057b0709095dfff742809a4b1eb
shonmikl <mikhailegorov676@gmail.com>
2023-10-18 11:25:30 +0200

    <>

--- src/main/java/org/example/_2023_10_16/generics/taskEx1/Team.java
+++ src/main/java/org/example/_2023_10_16/generics/taskEx1/Team.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ *
+ * @param <T> type
+ *
+ */
 public class Team<T extends Participant> {
     private String name;
     private List<T> participants = new ArrayList<>();
---
+++ src/main/java/org/example/_2023_10_18/StreamCollectExample.java
@@ -0,0 +1,7 @@
+package org.example._2023_10_18;
+
+public class StreamCollectExample {
+    public static void main(String[] args) {
+
+    }
+}
