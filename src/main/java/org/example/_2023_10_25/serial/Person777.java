ccc7a0bda5c36f7f783b4a81f939b3b5b375e0b2
shonmikl <mikhailegorov676@gmail.com>
2023-10-25 11:18:05 +0200

    IO

---
+++ src/main/java/org/example/_2023_10_25/serial/Person777.java
@@ -0,0 +1,29 @@
+package org.example._2023_10_25.serial;
+
+import java.io.Serializable;
+
+public class Person777 implements Serializable {
+    private String name;
+    private transient int age;
+
+    public Person777(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person777{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
