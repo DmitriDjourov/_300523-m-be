1484c8dbfbafa6fdecbdfb4356bc326e8bcfe73f
shonmikl <mikhailegorov676@gmail.com>
2023-10-25 11:53:30 +0200

    IO

--- src/main/java/org/example/_2023_10_25/serial/Main.java
+++ src/main/java/org/example/_2023_10_25/serial/Main.java
@@ -3,24 +3,26 @@ package org.example._2023_10_25.serial;
 import com.github.javafaker.Faker;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Person777 person777 = new Person777(new Faker().name().name(), 25);
-        System.out.println("Before serial: ");
-        System.out.println("Name: " + person777.getName());
-        System.out.println("Age: " + person777.getAge());
 
-        //Serial object to file
-        try {
-            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("person.ser"));
-            out.writeObject(person777);
-            out.close();
-            System.out.println("Serialization was done");
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        Person777 person777 = new Person777(new Faker().name().name(), 25);
+//        System.out.println("Before serial: ");
+//        System.out.println("Name: " + person777.getName());
+//        System.out.println("Age: " + person777.getAge());
+//
+//        //Serial object to file
+//        try {
+//            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("person.ser"));
+//            out.writeObject(person777);
+//            out.close();
+//            System.out.println("Serialization was done");
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
         Person777 newPerson;
 
