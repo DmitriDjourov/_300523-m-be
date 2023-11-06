2e4341ec02c4fbbc8acdde76b1242c1f970fd3c5
shonmikl <mikhailegorov676@gmail.com>
2023-11-06 11:00:03 +0100

    MOCK

---
+++ src/main/java/org/example/_2023_11_6/SimpleMockExample.java
@@ -0,0 +1,16 @@
+package org.example._2023_11_6;
+
+public class SimpleMockExample {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public String calculate(int a) {
+        int result = mult(10, 10) + sum(10, 10);
+        return "" + (result * a);
+    }
+}
