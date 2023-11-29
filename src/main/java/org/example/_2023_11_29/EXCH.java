139dc0f5a98d3b4d6f9a25ad37396aa412d01f54
shonmikl <mikhailegorov676@gmail.com>
2023-11-29 11:08:49 +0100

    sy

---
+++ src/main/java/org/example/_2023_11_29/EXCH.java
@@ -0,0 +1,71 @@
+package org.example._2023_11_29;
+
+import com.github.javafaker.Faker;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Exchanger;
+
+public class EXCH {
+    private final static Faker FAKER = new Faker();
+    public static void main(String[] args) {
+        Exchanger<Position> exchanger = new Exchanger<>();
+
+        List<Position> mainPlayerList = new ArrayList<>();
+        mainPlayerList.add(Position.SCISSORS);
+        mainPlayerList.add(Position.STONE);
+        mainPlayerList.add(Position.SCISSORS);
+        mainPlayerList.add(Position.PAPER);
+        mainPlayerList.add(Position.SCISSORS);
+
+        List<Position> anotherPlayerList = new ArrayList<>();
+        anotherPlayerList.add(Position.STONE);
+        anotherPlayerList.add(Position.PAPER);
+        anotherPlayerList.add(Position.SCISSORS);
+        anotherPlayerList.add(Position.STONE);
+        anotherPlayerList.add(Position.PAPER);
+
+        new Player(FAKER.name().name(), mainPlayerList, exchanger);
+        new Player(FAKER.name().name(), anotherPlayerList, exchanger);
+    }
+
+}
+
+enum Position {
+    STONE, SCISSORS, PAPER
+}
+
+class Player extends Thread {
+    private String name;
+    private List<Position> positions;
+    private Exchanger<Position> exchanger;
+
+    public Player(String name, List<Position> positions, Exchanger<Position> exchanger) {
+        this.name = name;
+        this.positions = positions;
+        this.exchanger = exchanger;
+        this.start();
+    }
+
+    private void play(Position mainPlayer, Position anotherPlayer) {
+        if(mainPlayer == Position.PAPER && anotherPlayer == Position.STONE ||
+           mainPlayer == Position.STONE && anotherPlayer == Position.SCISSORS ||
+           mainPlayer == Position.SCISSORS && anotherPlayer == Position.PAPER) {
+            System.out.println("***[ " + name + " ]: WINNER!***");
+        }
+    }
+
+    @Override
+    public void run() {
+        Position reply;
+        for (Position p : positions) {
+            try {
+                reply = exchanger.exchange(p);
+                play(p, reply);
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
